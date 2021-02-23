4812953bc3f9e9d581d71035829c22bf46398759
SteeveG <steeveguillot51@gmail.com>
2021-02-23 15:21:55 +0100

    Poste Seeder

---
+++ ppe4laravel/database/seeds/PosteSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Type;
+use App\Categorie;
+use App\User;
+
+class PosteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('postes')->insert ([
+            'intitule' => "Chef de projet",
+            'description' => "Tu fais du dev",
+            'ville' => "Y",
+            'nomEntreprise' => "Croc'Emploi",
+            'pdf' => "pdf emploi",
+            'isValide' => 1,
+            'type_id' => Type::all()->find(3)->id,
+            'categorie_id' => Categorie::all()->find(3)->id,
+            'user_id' => User::all()->find(5)->id,
+        ]);
+    }
+}
