0cfa6d0f397dd4997735ff8b5512db6c2090073e
SteeveG <steeveguillot51@gmail.com>
2021-02-25 18:59:34 +0100

    Suppression route

--- ppe4laravel/routes/web.php
+++ ppe4laravel/routes/web.php
@@ -16,8 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ('PosteController@index'));
 Route::resource('poste', 'PosteController');
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::resource('user','UserController');
