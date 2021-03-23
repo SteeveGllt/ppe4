d3c25ac01e34d3520b89bb133676ecb92af2ead9
Guillaume Ponsot <ponsotguillaume@gmail.com>
2021-03-23 15:00:28 +0100

    Merge origin/master
    Conflicts:
    	ppe4laravel/app/Http/Controllers/Controller.php
    	ppe4laravel/routes/web.php

--- ppe4laravel/app/Http/Controllers/Controller.php
+++ ppe4laravel/app/Http/Controllers/Controller.php
@@ -7,8 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-use App\Poste;
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
