6e46ed829132daaf05d0581662e464b385200a10
RonanSASSARD <79464735+RonanSASSARD@users.noreply.github.com>
2021-03-24 09:25:56 +0100

    Merge origin/master

--- ppe4laravel/app/Http/Controllers/Controller.php
+++ ppe4laravel/app/Http/Controllers/Controller.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-use App\Poste;
 
 class Controller extends BaseController
 {
